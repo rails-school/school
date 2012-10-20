abc2dffc553667e51f7772b0bb1bd6962e2879db
Michael Makarov <info1117@gmail.com>
2012-10-19 17:31:10 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Rs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
