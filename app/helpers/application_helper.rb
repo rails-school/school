fe5b13cebefec2fba0ec7df3c2a7bf0d1e9d0578
Michael Makarov <info1117@gmail.com>
2012-10-31 18:01:15 -0700

    select impemetation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 module ApplicationHelper
 
   def lessons_today?(year, month, day)
-    lessons = []
-    all = all_lessons.each do |l|
-      if l.date.month == month && l.date.day == day
-        lessons << l
-      end
-    end
-
-    lessons
-
+    all_lessons.select { |l| l.date.month == month && l.date.day == day }
   end
 end
