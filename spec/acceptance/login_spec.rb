fcf136937c9b4a0a32fb64c61ffd853ae2ee8386
Michael Makarov <info1117@gmail.com>
2012-11-12 17:12:30 -0800

    devise message and test for it

--- spec/acceptance/login_spec.rb
+++ spec/acceptance/login_spec.rb
@@ -6,6 +6,7 @@ feature %q{
   So other users can log in
 } do
 
+
   background do
     @user = FactoryGirl.create(:user)
     visit "/"
@@ -19,6 +20,31 @@ feature %q{
   scenario "Log out by clicking link", :js => true do
     click_link "Logout"
     page.should have_link("Login")
+  end
+
+
+end
+
+
+
+feature %q{
+  As a user
+  If I am mistaken with the password
+  I should see that message
+} do
+
+  background do
+    @user = FactoryGirl.create(:user)
+    visit "/"
+    click_link "Login"
+    fill_in "user[email]", :with => "some_random_mail@example.com"
+    fill_in "user[password]", :with => "draft1"
+    click_button "Sign in"
+  end
+
+
+  scenario "see the message", :js => true do
+    page.should have_content("Invalid email or password.")
   end
 
 
