2d1f1a7f526fc6868ad02c327c4054571b06741e
Michael Makarov <info1117@gmail.com>
2012-11-26 12:32:03 -0800

    adds html_safe to polls

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,7 +2,7 @@ module QuestionsHelper
   def answer_list_item(form, answer)
     if answer != "!"
       content_tag :li,
-        "#{form.radio_button(:text, answer)} #{answer}"
+        "#{form.radio_button(:text, answer)} #{answer}".html_safe
     end
   end
 end
