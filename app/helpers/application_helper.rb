acc5b433dc7fb0217917b450a782e5edc3dc3bb4
Gabe Kopley <gabe@coshx.com>
2012-11-28 20:49:39 -0800

    Edit copy on /about and /contact, some page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def lessons_today?(year, month, day)
     Lesson.all.select { |l| l.date.month == month && l.date.day == day }
   end
+
+  def title_content(page_title)
+    site_name = 'Rails School San Francisco'
+    page_title.present? ? "#{page_title} | #{site_name}" : site_name
+  end
 end
