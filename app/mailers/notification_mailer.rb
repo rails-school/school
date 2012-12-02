c90dd45d92bd72a0a26f92ebf4809cc2270ce54f
Michael Makarov <info1117@gmail.com>
2012-12-02 15:45:22 -0800

    Revert "Revert "Adds mail notifications""
    This reverts commit 25eaceff1c5441159c94e213756ca8258583db9a.

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ActionMailer::Base
+  default from: "info@railsschool.org"
+
+  def lesson_notification(lesson, user, current_user)
+    @lesson = lesson
+    @user = user
+    mail(:to => user.email, :subject => "Rails School SF new class on #{lesson.date}: #{lesson.title}", :from => current_user.email)
+  end
+
+end
