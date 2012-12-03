27be184ec905fb19e3b4e6a46442d862fb6621e0
Gabe Kopley <gabe@coshx.com>
2012-12-02 16:56:49 -0800

    Consolidate internals controller into home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,18 @@
 class HomeController < ApplicationController
-  def index
-    @lessons = Lesson.all
-    @past_lessons = Lesson.past_lessons
-    @future_lessons = Lesson.future_lessons
-  end
 
   def about
   end
 
   def calendar
+  end
+
+  def contact
+  end
+
+  def index
+    @lessons = Lesson.all
+    @past_lessons = Lesson.past_lessons
+    @future_lessons = Lesson.future_lessons
   end
 
 end
