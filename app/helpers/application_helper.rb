a5810f946b52120ba71913f0e03b0bc72b109b93
Gabe Kopley <gabe@coshx.com>
2012-12-02 16:32:17 -0800

    Format dates consistently

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,17 @@ module ApplicationHelper
     site_name = 'Rails School San Francisco'
     page_title.present? ? "#{page_title} | #{site_name}" : site_name
   end
+
+  def format_date(date)
+    date.try(:strftime, "%B %e, %Y")
+  end
+
+  def format_time(time)
+    t = time.try(:strftime, "%I:%M%p").downcase
+    t.sub(':00', '')
+  end
+
+  def format_datetime(date)
+    "#{format_time(date)} on #{format_date(date)}"
+  end
 end
