e59fc95164019ee25d0ab50f32d5e23e9a0bb39e
Gabe Kopley <gabe@coshx.com>
2012-12-13 18:13:43 -0800

    Fix bug.  Good thing we have our own pad server now :)

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,9 +1,9 @@
 module LessonsHelper
   def etherpad_host(lesson)
     case
-    when lesson.created_at > Time.parse("2012-11-07")
+    when lesson.date > Time.parse("2012-11-07")
       "http://pad.railsschool.org/p/"
-    when lesson.created_at > Time.parse("2012-08-31")
+    when lesson.date > Time.parse("2012-08-31")
       "http://beta.etherpad.org/p/"
     else
       "http://typewith.me/p/"
