3d9c92493e1d593dea56dc86cb238e93ccdbde0b
Gabe Kopley <gabe@coshx.com>
2012-12-26 17:42:27 -0800

    Plaintext email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,15 +20,8 @@ module ApplicationHelper
   def format_datetime(date)
     "#{format_time(date)} on #{format_date(date)}"
   end
-  def unsubscribe_url(user)
-    content_tag :a, "Unsubscribe link", :href => "#{root_url}unsubscribe/#{user.unsubscribe_token}"
-  end
 
   def notify_subscribers_link(lesson)
     content_tag :a, "notify subscribers", :href => "#{root_url}notify_subscribers/#{lesson.id}", 'data-method' => 'POST', 'data-remote' => true
-  end
-
-  def lesson_url(lesson)
-    content_tag :a, lesson.title, :href => "#{root_url}l/#{lesson.slug}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rs::Application.routes.draw do
     end
   end
 
-  get "unsubscribe/:code" => "users#unsubscribe"
+  get "unsubscribe/:code" => "users#unsubscribe", :as => "unsubscribe"
   get "about" => "home#about"
   get "calendar" => "home#calendar"
   get "contact" => "home#contact"
