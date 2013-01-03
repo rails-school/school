a4476fd5c7d616a7cd99175a901a42783ec2ed5a
Gabe Kopley <gabe@coshx.com>
2013-01-02 18:09:01 -0800

    Clean up helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,8 @@
 module QuestionsHelper
   def answer_list_item(form, answer)
-      content_tag :li,
-        "#{form.radio_button(:id, answer.id)} #{answer.text}".html_safe
+    content_tag :li do
+      form.radio_button(:id, answer.id) +
+      answer.text
+    end
   end
 end
