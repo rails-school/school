8a4c731857ded813a357de2e38e2dace33988ad6
Michael Makarov <info1117@gmail.com>
2013-01-04 12:25:01 -0800

    No default options for polls

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,8 @@
 module QuestionsHelper
   def answer_list_item(form, answer)
+    options = {:checked => false}
     content_tag :li do
-      form.radio_button(:id, answer.id) +
+      form.radio_button(:id, answer.id, options) +
       answer.text
     end
   end
