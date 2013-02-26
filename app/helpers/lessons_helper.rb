58e372776d33a8faa4873684f78a485ed66f120f
Gabe Kopley <gabe@coshx.com>
2013-02-26 11:08:07 -0800

    Fix whiteboard bug I introduced

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,9 +1,9 @@
 module LessonsHelper
   def etherpad_host(lesson)
     case
-    when lesson.date > Time.parse("2012-11-07")
+    when lesson.start_time > Time.parse("2012-11-07")
       "http://pad.railsschool.org/p/"
-    when lesson.date > Time.parse("2012-08-31")
+    when lesson.start_time > Time.parse("2012-08-31")
       "http://beta.etherpad.org/p/"
     else
       "http://typewith.me/p/"
