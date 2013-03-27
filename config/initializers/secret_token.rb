0b3cc0d0c3fe4bb0604f8baa86b8670a72dbf335
Gabe Kopley <gabe@coshx.com>
2013-03-26 20:04:41 -0700

    Look at environment variable for secret key in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rs::Application.config.secret_token = '6c962917b0a99394fc59a3a53a125529e8a212a084ec4dff2bb281869f5594f3b2f7b8d38a3aaf82f2dcd4c2f2f5711a56e5f3dbc2ac5ea66e61d12ddfca6d6e'
+if Rails.env.production?
+  Rs::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+else
+  Rs::Application.config.secret_token = "Blueberry23"
+end
