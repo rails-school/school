31b904911c676fbfd6c00f34e60b315a5aca2629
Gabe Kopley <gabe@coshx.com>
2013-08-12 14:52:42 -0700

    Bump contenteditable gem in order to pass new spec

---
+++ spec/acceptance/edit_teacher_bio_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+
+feature %q{
+  As a teacher
+  I want to be able to edit my bio
+  So I can tell the world something about me
+} do
+
+  background do
+    @admin = FactoryGirl.create(:admin)
+    sign_in_manually @admin
+  end
+
+  scenario "edit my bio", :js => true do
+    visit about_path
+    selector = "*[data-tag='about mike'][contenteditable]"
+    page.should have_css(selector)
+    puts %Q{$("#{selector}").text("Mike is a superhero")}
+    page.execute_script(%Q{$("#{selector}").text("Mike is a superhero")})
+    page.execute_script("$('.contenteditable_save_button').show();")
+    find(".contenteditable_save_button").click
+    find(selector).text.should == "Mike is a superhero"
+    visit about_path
+    find(selector).text.should == "Mike is a superhero"
+  end
+
+end
