89d13cbeecafedc68dd8fbb59c5d2537cd1830f3
Gabe Kopley <gabe@coshx.com>
2014-02-08 15:57:15 -0800

    Remember me cookie valid for 1 year, and renew on every new session

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -104,10 +104,10 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 1.year
 
   # If true, extends the user's remember period when remembered via cookie.
-  # config.extend_remember_period = false
+  config.extend_remember_period = true
 
   # Options to be passed to the created cookie. For instance, you can set
   # :secure => true in order to force SSL only cookies.
