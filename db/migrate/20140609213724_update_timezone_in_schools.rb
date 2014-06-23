8c22a1ac7c34a71868aba00115a2d30fe8ccae71
Florent Bouquet <florent.b.bouquet@orange.fr>
2014-06-23 13:27:59 -0700

    Fixes indentation

--- db/migrate/20140609213724_update_timezone_in_schools.rb
+++ db/migrate/20140609213724_update_timezone_in_schools.rb
@@ -1,12 +1,12 @@
 class UpdateTimezoneInSchools < ActiveRecord::Migration
   def change
-  	sf_school = School.find_by_slug("sf")
-  	unless sf_school.nil?
+    sf_school = School.find_by_slug("sf")
+    unless sf_school.nil?
       sf_school.timezone = 'Pacific Time (US & Canada)'
       sf_school.save
     end
 
-  	cville_school = School.find_by_slug("cville")
+    cville_school = School.find_by_slug("cville")
     unless cville_school.nil?
       cville_school.timezone = 'Eastern Time (US & Canada)'
       cville_school.save
