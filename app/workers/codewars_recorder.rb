08d22764adefb20980fa1e2ba8b499c379a69511
nbhartiya <neeharika@alum.mit.edu>
2015-01-02 11:54:48 -0800

    makes codewars.com http request http to avoid a redirect

--- app/workers/codewars_recorder.rb
+++ app/workers/codewars_recorder.rb
@@ -5,7 +5,7 @@ class CodewarsRecorder
   def perform(user_id, codewars_username)
     user = User.includes(:codewars).find(user_id)
     user_codewar_slugs = user.codewars.map(&:slug)
-    codewars = HTTParty.get("https://www.codewars.com/api/v1/users/#{codewars_username}/code-challenges/completed")['data']
+    codewars = HTTParty.get("http://www.codewars.com/api/v1/users/#{codewars_username}/code-challenges/completed")['data']
     codewars.each do |codewar|
       unless user_codewar_slugs.include?(codewar["slug"])
         user.codewars.create(:slug => codewar["slug"])
