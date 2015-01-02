b5547a4c35f003a4f66215b0672df6d8962434ad
nbhartiya <neeharika@alum.mit.edu>
2015-01-02 15:34:34 -0800

    refactors codewarrior spec, as there was no need for variables

--- spec/models/badges/codewarrior_spec.rb
+++ spec/models/badges/codewarrior_spec.rb
@@ -4,23 +4,12 @@ describe Badge::Codewarrior do
 
   describe ".allocate_to_user?" do
     let(:user) { create(:user) }
-    let(:codewar1) {create(:codewar, user: user)}
-    let(:codewars5) do
-      5.times.map { |i|
-        create(:codewar, user: user)
-      }
-    end
-    let(:codewars7) do
-      7.times.map { |i|
-        create(:codewar, user: user)
-      }
-    end
 
     subject { Badge::Codewarrior.allocate_to_user?(user) }
 
     context "user did not complete 5 codewars challenges" do
       before do
-        codewar1
+        create(:codewar, user: user)
       end
 
       it { should be_false }
@@ -29,7 +18,9 @@ describe Badge::Codewarrior do
 
     context "user completed exactly 5 codewar challenges" do
       before do
-        codewars5
+        5.times.map { |i|
+          create(:codewar, user: user)
+        }
       end
 
       it { should be_true }
@@ -37,7 +28,9 @@ describe Badge::Codewarrior do
 
     context "user completed more than 5 codewar challenges" do
       before do
-        codewars7
+        7.times.map { |i|
+          create(:codewar, user: user)
+        }
       end
 
       it { should be_true }
