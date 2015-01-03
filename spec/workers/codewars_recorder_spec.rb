b2f4ddd36ad837cf25debb1b99f92069152f9d72
nbhartiya <neeharika@alum.mit.edu>
2015-01-02 17:24:34 -0800

    adds test for codewars_recorder worker

---
+++ spec/workers/codewars_recorder_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'httparty'
+
+describe CodewarsRecorder do
+  describe ".perform_async" do
+    let(:user) { create(:user, codewars_username:"nbhartiya") }
+    let!(:codewars_count) {
+      codewars_count = 0
+      codewars_completed = HTTParty.get("http://www.codewars.com/api/v1/users/nbhartiya/code-challenges/completed")['data']
+      codewars_completed.each do |exercise|
+        exercise["completedLanguages"].each do |language|
+          codewars_count += 1
+        end
+      end
+      codewars_count
+    }
+    
+
+
+    context "if the user has a codewars_username" do
+      context "but already has one codewar populated" do
+        let!(:codewar) {
+          create(:codewar, user_id: user.id, slug: "multiply", language: "clojure")
+        }
+
+        it "creats new codewars that are not already existing, resulting in correct codewars count" do
+          CodewarsRecorder.perform_async(user.id, user.codewars_username)
+          user.codewars.count.should == codewars_count
+        end
+      end
+
+      context "and has no codewars populated" do
+        it "creates all new codewars, resulting in correct codewars count" do
+          CodewarsRecorder.perform_async(user.id, user.codewars_username)
+          user.codewars.count.should == codewars_count
+        end
+      end
+    end
+  end
+end
