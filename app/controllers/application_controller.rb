5e23423fc10165bc0aa7e3d438548d57c0b73988
nbhartiya <neeharika@alum.mit.edu>
2015-01-05 17:54:36 -0800

    checks codewars completed everytime user signs in so long as it hasnt been checked in last 30 minutes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,9 +71,9 @@ class ApplicationController < ActionController::Base
 
   def maybe_enqueue_codewars_recorder
     return unless user_signed_in?
-    return unless current_user.teacher?
-    # Below: Checks codewars completed for the teacher.
+    # Below: Checks codewars completed for logged in user.
     enqueue_codewars_recorder(current_user)
+    return unless current_user.teacher?
     # Below: Checks the codewars completed for each of the students of this teacher's upcoming lessons (in next 1 week).
     upcoming_lessons_for_teacher = Lesson.includes(:users)
                          .where("start_time > ? AND start_time < ? AND teacher_id = ?", Time.current, Time.current + 1.week, current_user.id)
@@ -91,7 +91,7 @@ class ApplicationController < ActionController::Base
   def enqueue_codewars_recorder(student)
     return unless student.codewars_username.present?
     if student.last_codewars_checked_at.nil? ||
-        (Time.now - student.last_codewars_checked_at > 3600)
+        (Time.now - student.last_codewars_checked_at > 30.minutes)
       CodewarsRecorder.perform_async(student.id, student.codewars_username)
       student.last_codewars_checked_at = Time.now
       student.save!
