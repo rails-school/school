d406d90d252967074b2445a805112ed7e2808bce
Brad Johnson <cdale77@gmail.com>
2015-03-09 16:35:23 -0700

    fix spec

--- spec/acceptance/lessons_spec.rb
+++ spec/acceptance/lessons_spec.rb
@@ -318,7 +318,8 @@ feature %q{
 
   scenario "Visiting the page directly by its URL" do
     visit lesson_path(@lesson)
-    page.should have_content("Be sure to complete the following codewars challenge before class! http://www.codewars.com/kata/multiply/train/ruby")
+    page.should have_content("Be sure to complete the following codewars challenge before class! " \
+      "(If your RailsSchool profile includes your codewars username, you'll get a nifty star if complete the challenge.) http://www.codewars.com/kata/multiply/train/ruby")
   end
 
 end
