b49aecd0456817d707e1780f1ff1d3f2935ac995
nbhartiya <neeharika@alum.mit.edu>
2015-03-10 15:44:35 -0700

    removes binding.pry, removes extra spacing to appease Hound

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -46,42 +46,43 @@ class Lesson < ActiveRecord::Base
     calendar.ip_method = "REQUEST"
     calendar.events << Icalendar::Event.new
     if self.venue.school.timezone.include?("Pacific")
-      # Makes calendar event in Pacfic Time (Daylight / Standard depending on time of year)
+      # Makes calendar event in Pacfic Time
+      # (Daylight / Standard depending on time of year)
       calendar.timezone do |t|
         t.tzid = "America/Los_Angeles"
         t.daylight do |d|
           d.tzoffsetfrom = "-0800"
-          d.tzoffsetto   = "-0700"
-          d.tzname       = "PDT"
-          d.dtstart      = "19700308T020000"
-          d.rrule        = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
+          d.tzoffsetto = "-0700"
+          d.tzname = "PDT"
+          d.dtstart = "19700308T020000"
+          d.rrule = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
         end
         t.standard do |s|
           s.tzoffsetfrom = "-0700"
-          s.tzoffsetto   = "-0800"
-          s.tzname       = "PST"
-          s.dtstart      = "19701101T020000"
-          s.rrule        = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
+          s.tzoffsetto = "-0800"
+          s.tzname = "PST"
+          s.dtstart = "19701101T020000"
+          s.rrule = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
         end
       end
     elsif self.venue.school.timezone.include?("Eastern")
-      binding.pry
-      # Makes calendar event in Eastern Time (Daylight / Standard depending on time of year)
+      # Makes calendar event in Eastern Time
+      # (Daylight / Standard depending on time of year)
       calendar.timezone do |t|
         t.tzid = "America/New_York"
         t.daylight do |d|
           d.tzoffsetfrom = "-0500"
-          d.tzoffsetto   = "-0400"
-          d.tzname       = "EDT"
-          d.dtstart      = "19700308T020000"
-          d.rrule        = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
+          d.tzoffsetto = "-0400"
+          d.tzname = "EDT"
+          d.dtstart = "19700308T020000"
+          d.rrule = "FREQ=YEARLY;BYMONTH=3;BYDAY=2SU"
         end
         t.standard do |s|
           s.tzoffsetfrom = "-0400"
-          s.tzoffsetto   = "-0500"
-          s.tzname       = "EST"
-          s.dtstart      = "19701101T020000"
-          s.rrule        = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
+          s.tzoffsetto = "-0500"
+          s.tzname = "EST"
+          s.dtstart = "19701101T020000"
+          s.rrule = "FREQ=YEARLY;BYMONTH=11;BYDAY=1SU"
         end
       end
     end
