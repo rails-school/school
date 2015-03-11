f95f19727634b4bf4f6936bc06b9ac462840728c
Brad Johnson <cdale77@gmail.com>
2015-03-11 10:25:36 -0700

    jar jar badge

---
+++ app/models/badges/jar_jar_binks.rb
@@ -0,0 +1,21 @@
+class Badge::JarJarBinks
+  BADGE_ID = 4
+  DISPLAY_NAME = "Jar Jar Binks"
+  SLUG = "jarjarbinks"
+
+  include Badge
+
+  def description
+    "Given to users who have attended at least one lesson."
+  end
+
+  def notification_bonus_message
+    "Welcome to RailsSchool! We hope you enjoyed your first class"
+  end
+
+  def self.allocate_to_user?(user)
+    user.attendances.count == 1
+  end
+
+end
+
