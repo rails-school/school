7eb3a2c38c4dfaf4f95af767db7c50dfa80e47c1
Brad Johnson <cdale77@gmail.com>
2015-03-11 13:51:06 -0700

    user badges complete

---
+++ spec/models/badges/boba_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Badge::BobaFett do
+
+  describe ".allocate_to_user?" do
+    let(:user) { create(:user) }
+
+    subject { Badge::BobaFett.allocate_to_user?(user) }
+
+    context "user did not attend any classes" do
+      it { should eq(false) }
+    end
+
+    context "user attended less than 35 classes" do
+      before { 34.times { create(:attendance, user: user) } }
+      it { should eq(false) }
+    end
+
+    context "user attended exactly 35 classes" do
+      before { 35.times { create(:attendance, user: user) } }
+      it { should eq(true) }
+    end
+
+    context "user attended more than 35" do
+      before { 36.times { create(:attendance, user: user) } }
+      it { should eq(true) }
+    end
+  end
+end
+
