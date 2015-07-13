e56140fb5f505f33781118b31ef80d7ec2791dfe
Adrien Cadet <acadet@live.fr>
2015-07-13 14:35:54 -0700

    Uses logger instead of stdout

--- config/initializers/ios_apn.rb
+++ config/initializers/ios_apn.rb
@@ -5,14 +5,14 @@ if Rails.env.production?
   if File.exist? certificate
     IOS_APN.certificate = File.read(certificate)
   else
-    puts "iOS APN: #{certificate} not found"
+    logger.info "iOS APN: #{certificate} not found"
   end
 
   passphrase = "config/ios_production_passphrase.txt"
   if File.exist? passphrase
     IOS_APN.passphrase = File.read(passphrase)
   else
-    puts "iOS APN: #{passphrase} not found"
+    logger.info "iOS APN: #{passphrase} not found"
   end
 else
   IOS_APN = ENV["IOS_APN"] || Houston::Client.development
@@ -21,13 +21,13 @@ else
   if File.exist? certificate
     IOS_APN.certificate = File.read(certificate)
   else
-    puts "iOS APN: #{certificate} not found"
+    logger.info "iOS APN: #{certificate} not found"
   end
 
   passphrase = "config/ios_development_passphrase.txt"
   if File.exist? passphrase
     IOS_APN.passphrase = File.read(passphrase)
   else
-    puts "iOS APN: #{passphrase} not found"
+    logger.info "iOS APN: #{passphrase} not found"
   end
 end
