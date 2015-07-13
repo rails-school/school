f50507a647cfd1f8b5a35a5f0db518567891a3a4
Adrien Cadet <acadet@live.fr>
2015-07-13 11:26:14 -0700

    Adds production certificate

--- config/initializers/ios_apn.rb
+++ config/initializers/ios_apn.rb
@@ -1,9 +1,9 @@
 if Rails.env.production?
   IOS_APN = ENV['IOS_APN'] || Houston::Client.production
-  IOS_APN.certificate = File.read('config/ios_production_certificate.txt')
+  IOS_APN.certificate = File.read('config/ios_production_certificate.pem')
   IOS_APN.passphrase = File.read('config/ios_production_passphrase.txt')
 else
   IOS_APN = ENV['IOS_APN'] || Houston::Client.development
-  IOS_APN.certificate = File.read('config/ios_development_certificate.txt')
+  IOS_APN.certificate = File.read('config/ios_development_certificate.pem')
   IOS_APN.passphrase = File.read('config/ios_development_passphrase.txt')
 end
