d5de04641a15ad82a4e5c6f20f0837f30f28afb2
Adrien Cadet <acadet@live.fr>
2015-07-13 14:37:50 -0700

    Removes logger uses

--- config/initializers/ios_apn.rb
+++ config/initializers/ios_apn.rb
@@ -4,15 +4,11 @@ if Rails.env.production?
   certificate = "config/ios_production_certificate.pem"
   if File.exist? certificate
     IOS_APN.certificate = File.read(certificate)
-  else
-    logger.info "iOS APN: #{certificate} not found"
   end
 
   passphrase = "config/ios_production_passphrase.txt"
   if File.exist? passphrase
     IOS_APN.passphrase = File.read(passphrase)
-  else
-    logger.info "iOS APN: #{passphrase} not found"
   end
 else
   IOS_APN = ENV["IOS_APN"] || Houston::Client.development
@@ -20,14 +16,10 @@ else
   certificate = "config/ios_development_certificate.pem"
   if File.exist? certificate
     IOS_APN.certificate = File.read(certificate)
-  else
-    logger.info "iOS APN: #{certificate} not found"
   end
 
   passphrase = "config/ios_development_passphrase.txt"
   if File.exist? passphrase
     IOS_APN.passphrase = File.read(passphrase)
-  else
-    logger.info "iOS APN: #{passphrase} not found"
   end
 end
